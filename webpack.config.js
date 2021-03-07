c2855ee9d919ec9865d030efc1497cc72e5a9b8b
RomanDovgii <romandovgiiwork@gmail.com>
2021-03-07 09:23:56 +0300

    Improved consistency in the config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const SplitByPathPlugin = require('split-by-path-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const fs = require('fs');
-const path = require('path');
+const HtmlWebpackPlugin = require(`html-webpack-plugin`);
+const SplitByPathPlugin = require(`split-by-path-webpack-plugin`);
+const CopyPlugin = require(`copy-webpack-plugin`);
+const fs = require(`fs`);
+const path = require(`path`);
 
 function generateHtmlPlugins(templateDir) {
     const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
 
     return templateFiles.map((item) => {
-        const parts = item.split('.');
+        const parts = item.split(`.`);
         const name = parts[0];
         const extension = parts[1];
 
@@ -23,46 +23,46 @@ function generateHtmlPlugins(templateDir) {
 const htmlPlugins = generateHtmlPlugins(`./src/html`);
 
 module.exports = {
-    mode: 'development',
+    mode: `development`,
     entry: [
-        './src/js/index.js',
-        './src/scss/style.scss'
+        `./src/js/index.js`,
+        `./src/scss/style.scss`
     ],
     output: {
-        filename: './js/bundle.js',
-        path: path.resolve(__dirname, 'public')
+        filename: `./js/bundle.js`,
+        path: path.resolve(__dirname, `public`)
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
+        contentBase: path.resolve(__dirname, `public`),
         open: false,
         port: 8089,
         historyApiFallback: true,
-        contentBase: path.resolve(__dirname, '/public'),
+        contentBase: path.resolve(__dirname, `/public`),
         watchContentBase: true,
     },
-    devtool: "source-map",
+    devtool: `source-map`,
     module: {
         rules:
         [
             {
                 test: /\.(js|jsx)$/,
-                include: path.resolve(__dirname, 'src/js'),
+                include: path.resolve(__dirname, `src/js`),
                 exclude: /node_modules/,
                 use: {
-                    loader: 'babel-loader',
+                    loader: `babel-loader`,
                 }
             },
             {
                 test: /\.(sass|scss)$/,
-                include: path.resolve(__dirname, 'src/scss'),
+                include: path.resolve(__dirname, `src/scss`),
                 use: [
                     {
-                        loader: "file-loader",
-                        options: {outputPath: 'css/', name: 'style.min.css'}
+                        loader: `file-loader`,
+                        options: {outputPath: `css/`, name: `style.min.css`}
                     },
-                    "sass-sourcemap-loader",
+                    `sass-sourcemap-loader`,
                     {
-                      loader: "sass-loader",
+                      loader: `sass-loader`,
                       options: {
                         sourceMap: true,
                       },
@@ -72,26 +72,26 @@ module.exports = {
             {
                 test: /\.html$/,
                 include: path.resolve(__dirname, `src/html/includes`),
-                use: ['raw-loader']
+                use: [`raw-loader`]
             },
         ]
     },
     resolve: {
-        extensions: ['.js', '.jsx']
+        extensions: [`.js`, `.jsx`]
     },
     plugins: [
 
         new CopyPlugin({
           patterns: [
-            //remove this part if you don't need to copy fonts
+            //remove this part if you don`t need to copy fonts
             {
-              from: path.resolve(__dirname, "src/fonts"),
-              to: path.resolve(__dirname, "public/fonts/"),
+              from: path.resolve(__dirname, `src/fonts`),
+              to: path.resolve(__dirname, `public/fonts/`),
             },
-            // remove this part if you don't need to copy images
+            // remove this part if you don`t need to copy images
             {
-              from: path.resolve(__dirname, "src/img"),
-              to: path.resolve(__dirname, "public/img/"),
+              from: path.resolve(__dirname, `src/img`),
+              to: path.resolve(__dirname, `public/img/`),
             }
           ],
         }),
